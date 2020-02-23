203850edd028fd2a70ef719ccd419ddc78bfec88
Tyson Morris <temorris87@yahoo.com>
2020-02-23 11:20:52 -0500

    File transmission implemented.

--- application.py
+++ application.py
@@ -1,11 +1,28 @@
-from flask import Flask
+import os
+from flask import Flask, flash, request, redirect
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'C:\\Users\\temor\\Desktop'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 application = Flask(__name__)
+application.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @application.route('/')
 def index():
-    return "Hello, World! Adding additional text for testing CICD."
+    file = request.files['file']
+    if file.filename == '':
+        return 'Image not found.'
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(application.config['UPLOAD_FOLDER'], filename + '_test'))
+        return 'Image saved successfully.'
 
 
 if __name__ == '__main__':
